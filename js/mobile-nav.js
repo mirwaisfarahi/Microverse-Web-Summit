ef25e5cb565d21080ce4f1fde7dc9f691e7d5d83
Mirwais Farahi <waisfarahi@gmail.com>
2022-05-26 19:51:34 +0500

    Edit mobile-nav.js File

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -2,18 +2,18 @@ const bar = document.querySelector('#bar');
 const close = document.querySelector('#close');
 const nav = document.querySelector('.nav-list');
 
-if(bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-        close.classList.add('active');
-    });
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+    close.classList.add('active');
+  });
 }
 
-if(close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-        close.classList.remove('active');
-    });
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+    close.classList.remove('active');
+  });
 }
 
 const links = document.querySelectorAll('.link');
